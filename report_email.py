2bd8c3a4ec82fa4ba8ca96ffc5ce6a5838c5e825
Markus <markus.ark.klier@gmail.com>
2021-05-27 20:14:50 +0200

    Add scripts to process data into PDF and send them via email
    Use report_email.py to create a PDF containing the data which is then sent via email to the product supplier.

---
+++ report_email.py
@@ -0,0 +1,32 @@
+#!/usr/bin/#!/usr/bin/env python3
+
+import os
+from datetime import datetime
+import reports
+import emails
+
+
+def process_data():
+
+    data = []
+    for item in description_files:
+        items = [i.strip() for i in f.readlines()]
+        data.append(item[0] + '<br/>' + item[1] + '<br/><br/>')
+
+    return data
+
+def main():
+
+    description_files = '/supplier-data/descriptions/'
+    current_date = datetime.today().strftime('%Y-%m-%d')
+    paragraph_data = process_data(description_files)
+    title = 'Processed Update on ' + current_date
+    email_text = 'All fruits are uploaded to our website successfully. A detailed list is attached to this email.'
+
+    reports.generate_report('/tmp/processed.pdf', title, str(paragraph_data))
+
+    message = emails.generate('automation@example.com',  "{}@example.com".format(os.environ.get('USER')), 'Upload Completed - Online Fruit Store', email_text, '/tmp/processed.pdf')
+
+
+if __name__ == "__main__":
+        main()
